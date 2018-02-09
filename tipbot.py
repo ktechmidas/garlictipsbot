736a8d0173e88d5033b32e219e355f1ceb247630
Daniel <daniel@dashlabs.org>
2018-02-09 23:20:06 +0800

    updated message

--- tipbot.py
+++ tipbot.py
@@ -271,7 +271,7 @@ If you need any further assistance please PM my creator, /u/ktechmidas"""
             message.reply(self.help)
         elif command == "rates":
             amt = self.get_rates()
-            message.reply("The current rates for selling Garlic are:\n1 Dash = %s GRLC\n\nThe current rates for buying Garlic are:\n%s GRLC = 1 Dash" % (amt,amt))
+            message.reply("The current rate is \n[Dash/GRLC <>  %s]" % (amt))
         else:
             message.reply("Sorry! I did not understand the command you gave. Please write a new PM (not reply) with help and I will reply with what I accept.")
 
